6ae415b385051185159e32d865877bdfd5722a39
cristalcabrera95 <cristalcabrera9507@gmail.com>
2019-07-29 13:06:30 -0500

    Updates

---
+++ precache-manifest.62aca39de44e1ace971338df865b0917.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a45303207d6beb03eeb01a89f267970",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11c17bf9d3d1c1935059",
+    "url": "/static/css/main.103e49ad.chunk.css"
+  },
+  {
+    "revision": "e86a012dd8ec1ff550a4",
+    "url": "/static/js/2.840ef34b.chunk.js"
+  },
+  {
+    "revision": "11c17bf9d3d1c1935059",
+    "url": "/static/js/main.d838bdfb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
